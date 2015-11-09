7a7077bdacd7b6efec97222caa255002536bdd6e
Ra Mihaly <ra@mihaly.com>
2015-11-09 21:17:58 +0100

    Add vs sln

---
+++ AzureBlobStorage/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AzureBlobStorage.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AzureBlobStorage/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AzureBlobStorage.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
